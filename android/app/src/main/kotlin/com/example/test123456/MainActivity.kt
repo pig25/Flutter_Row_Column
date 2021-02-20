524902a957ad8f6474d1120a1e9204fa947bacc2
pig25 <relife25jw@gmail.com>
2021-02-20 23:25:34 +0800

    20210220

---
+++ android/app/src/main/kotlin/com/example/test123456/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test123456
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MyScaffold extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return row;
+  }
+}
+var col = Column(
+  children: [
+    row,
+    row,
+    row
+  ],
+);
+var row = Row(
+  // crossAxisAlignment: CrossAxisAlignment.center,
+  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: [
+    test,
+    Flexible(
+      fit: FlexFit.tight,
+      flex: 2,
+      child: test,
+    ),
+    Flexible(
+      fit: FlexFit.loose,
+      flex: 1,
+      child: test,
+    ),
+  ],
+);
+var test = Container(
+width: 100,
+  height: 100,
+  child: Text("123456789"),
+  decoration: BoxDecoration(
+    color: Colors.greenAccent,
+    border: Border.all()
+  ),
+
+);
+void main() {
+  runApp(MaterialApp(
+    title: 'My app', // used by the OS task switcher
+    home: SafeArea(
+      child: MyScaffold(),
+    ),
+  ));
+}
